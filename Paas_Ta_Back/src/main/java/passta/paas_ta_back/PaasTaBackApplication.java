bc248c36b74a60dd9b66b2ffc47048576be9f906
joohyun333 <joohyun333@naver.com>
2021-10-08 20:30:32 +0900

    init

---
+++ Paas_Ta_Back/src/main/java/passta/paas_ta_back/PaasTaBackApplication.java
@@ -0,0 +1,13 @@
+package passta.paas_ta_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaasTaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaasTaBackApplication.class, args);
+    }
+
+}
---
+++ Paas_Ta_Back/src/test/java/passta/paas_ta_back/PaasTaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package passta.paas_ta_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaasTaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
