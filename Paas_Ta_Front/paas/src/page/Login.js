d4d64138f8adc2be8698a070bde45ebaa65ca1fe
jungbada269 <57929751+jungbada269@users.noreply.github.com>
2021-10-09 17:29:42 +0900

    Update Login.js

--- Paas_Ta_Front/paas/src/page/Login.js
+++ Paas_Ta_Front/paas/src/page/Login.js
@@ -61,23 +61,22 @@ const onClickLogin = () => {
       console.log(res)
       console.log('res.data.userId :: ', res.data.user_email)
       console.log('res.data.msg :: ', res.data.msg)
-      if(res.data.user_email === undefined){
+      if(res.data.uemail === undefined){
           // id 일치하지 않는 경우 userId = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
           console.log('======================',res.data.msg)
-          //alert('입력하신 id 가 일치하지 않습니다.')
-          //alert('name : ', email);
-          console.log(res.data.name)
-      } else if(res.data.user_email === null){
+          alert('입력하신 id 가 일치하지 않습니다.')
+      } else if(res.data.email === null){
           // id는 있지만, pw 는 다른 경우 userId = null , msg = undefined
           console.log('======================','입력하신 비밀번호 가 일치하지 않습니다.')
           alert('입력하신 비밀번호 가 일치하지 않습니다.')
-      } else if(res.data.user_email === email) {
+      } else if(res.data.email === email) {
           // id, pw 모두 일치 userId = userId1, msg = undefined
           console.log('======================','로그인 성공')
-          sessionStorage.setItem('email', email)
+          alert(res.data.name,'님 환영합니다.')
+          //sessionStorage.setItem('email', email)
+          document.location.href = '/Town'
       }
       // 작업 완료 되면 페이지 이동(새로고침)
-      //document.location.href = '/Town'
   })
   .catch()
 }
