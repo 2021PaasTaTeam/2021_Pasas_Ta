9bbf6d9d40f350893f0f5eec2a1f101a7979428f
joohyun333 <joohyun333@naver.com>
2021-11-06 17:47:24 +0900

    소켓 추가

---
+++ Paas_Ta_Back/src/main/java/passta/paas_ta_back/websocket/WebSocketConfiguration.java
@@ -0,0 +1,16 @@
+package passta.paas_ta_back.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketHandler(), "/videochat").setAllowedOrigins("*");
+    }
+}
