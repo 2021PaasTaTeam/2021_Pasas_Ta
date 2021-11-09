cd08e8d8b536f0253731704aa2d203bb932301f3
joohyun333 <joohyun333@naver.com>
2021-11-09 20:02:20 +0900

    initDB 수정

--- Paas_Ta_Back/src/main/java/passta/paas_ta_back/initDb.java
+++ Paas_Ta_Back/src/main/java/passta/paas_ta_back/initDb.java
@@ -36,25 +36,24 @@ public class initDb {
             User admin = User.createUser("admin", "admin", "admin", "admin", UserType.ADMIN);
             em.persist(admin);
             //초기 사용자 세팅
-            User user2 = User.createUser("이름2", "email2", "1234", "주소1", UserType.CONSUMER);
-            em.persist(user2);
+            User consumer = User.createUser("consumer1", "consumer1@email.com", "1234", "서울특별시 강남구 개포동", UserType.CONSUMER);
+            em.persist(consumer);
             // 초기 가게 사용자 세팅
-            User user = User.createUser("이름3", "email3", "1234", "주소1", UserType.SELLER);
-            em.persist(user);
-
-            User seller1 = User.createUser("이름4", "email4", "1234", "주소4", UserType.SELLER);
+            User seller1 = User.createUser("seller1", "seller1@email.com", "1234", "경기도 안양시 동안구 호계3동", UserType.SELLER);
             em.persist(seller1);
-            User seller2 = User.createUser("이름5", "email5", "1234", "주소5", UserType.SELLER);
+            User seller2 = User.createUser("seller2", "seller2@email.com", "1234", "서울특별시 강동구 강일동", UserType.SELLER);
             em.persist(seller2);
-            User seller3 = User.createUser("이름6", "email6", "1234", "주소6", UserType.SELLER);
+            User seller3 = User.createUser("seller3", "seller3@email.com", "1234", "서울특별시 강북구 미아동", UserType.SELLER);
             em.persist(seller3);
+            User seller4 = User.createUser("seller4", "seller4@email.com", "1234", "서울특별시 영등포구 문래동1가", UserType.SELLER);
+            em.persist(seller4);
 
 
             List<String> locations = Arrays.asList("성북", "동작", "종로", "구로", "서초", "영등포");
             List<Land> lands = new ArrayList<>();
 
             //지역구 돌면서 세팅
-            for (int loc = 0 ; loc <6 ; loc ++){
+            for (int loc = 0; loc < 6; loc++) {
                 for (int y = 250; y <= 650; y += 400) {
                     for (int x = 200; x <= 1700; x += 600) {
                         //초기 Land 좌표 셋팅값
@@ -69,91 +68,91 @@ public class initDb {
 
 
             // 초기 가게 세팅
-            UploadFile uploadFile1 = new UploadFile("사용자파일명1", "uuid파일명1");
-            Shop shop1 = Shop.createShop(
-                    user,
-                    "사업자등록번호1",
-                    "가게1",
-                    "가게전화번호1",
-                    "가게지역명1",
-                    "가게상세주소1",
-                    "가게 타입1",
+            UploadFile uploadFile1 = new UploadFile("사용자파일명1.jpg", "uuid파일명1.jpg");
+            Shop teaShop = Shop.createShop(
+                    seller1,
+                    "201-03-47442",
+                    "수연생선방",
+                    "02-1122-1122",
+                    "성북구",
+                    "서울특별시 성북구 성북로26길",
+                    "생선",
                     uploadFile1,
                     lands.get(0));
-            em.persist(shop1);
+            em.persist(teaShop);
 
             UploadFile uploadFile2 = new UploadFile("사용자파일명2.jpg", "uuid파일명2.jpg");
 
 
-            Shop shop2 = Shop.createShop(
+            Shop bakerShop = Shop.createShop(
                     admin,
-                    "사업자등록번호2",
-                    "가게2",
-                    "가게전화번호2",
-                    "가게지역명2",
-                    "가게상세주소2",
-                    "가게 타입2",
+                    "201-03-47443",
+                    "성북동생선공장",
+                    "02-763-1234",
+                    "성북구",
+                    "서울특별시 성북구 대사관로 23",
+                    "생선",
                     uploadFile2,
                     lands.get(3));
-            em.persist(shop2);
+            em.persist(bakerShop);
 
 
-            Shop shop3 = Shop.createShop(
-                    seller2,
-                    "사업자등록번호3",
-                    "가게3",
-                    "가게전화번호3",
-                    "가게지역명3",
-                    "가게상세주소3",
-                    "가게 타입3",
+            Shop cafeShop = Shop.createShop(
+                    seller3,
+                    "201-03-47444",
+                    "알렉스더한복 성북점",
+                    "02-1242-1212",
+                    "성북구",
+                    "서울특별시 성북구 성북로28길",
+                    "한복",
                     uploadFile2,
                     lands.get(4));
-            em.persist(shop3);
+            em.persist(cafeShop);
 
 
             Shop shop4 = Shop.createShop(
-                    seller3,
-                    "3",
-                    "가게2",
-                    "가게전화번호2",
-                    "가게지역명2",
-                    "가게상세주소2",
-                    "가게 타입2",
+                    seller4,
+                    "201-03-47445",
+                    "LongBoat Smoker",
+                    "02-7072-1920",
+                    "성북구",
+                    "서울특별시 성북구 미아동",
+                    "생선",
                     uploadFile2,
                     lands.get(5));
             em.persist(shop4);
 
             // 초기 image list 세팅
             List<UploadFile> uploadFiles = new ArrayList<>();
-            UploadFile itemImage = new UploadFile("사용자파일명3", "uuid파일명3");
+            UploadFile itemImage = new UploadFile("item파일명.jpg", "itemUUID파일명.jpg");
             for (int i = 0; i < 3; i++) {
                 uploadFiles.add(itemImage);
             }
 
             // 초기 item 세팅
             Item item1 = Item.createItem(
-                    shop1,
-                    "상품1",
-                    "상품 내용",
-                    10000,
-                    10,
+                    teaShop,
+                    "고등어",
+                    "싱싱한 고등어, 눈이 반짝인다.",
+                    3000,
+                    27,
                     uploadFiles);
             em.persist(item1);
 
             Item item2 = Item.createItem(
-                    shop1,
-                    "상품2",
-                    "상품 내용",
+                    teaShop,
+                    "광어",
+                    "싱싱한 광어, 날 노려보고 있다.",
                     20000,
                     20,
                     uploadFiles);
             em.persist(item2);
 
             Item item3 = Item.createItem(
-                    shop1,
-                    "상품3",
-                    "상품 내용",
-                    30000,
+                    teaShop,
+                    "문어",
+                    "싱싱한 문어, 서로 팔씨름을 하고 있다.",
+                    80000,
                     30,
                     uploadFiles);
             em.persist(item3);
@@ -169,7 +168,7 @@ public class initDb {
             orderItems.add(orderItem1);
             orderItems.add(orderItem2);
             orderItems.add(orderItem3);
-            Order order = Order.createOrder(user, orderItems);
+            Order order = Order.createOrder(seller1, orderItems);
             order.finishOrder();
             em.persist(order);
         }
